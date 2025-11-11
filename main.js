2ed4136d3c50d9a7df9dc89fd6a79e202e1b3a42
evayuniar <evayuniar@gmail.com>
2025-11-11 10:49:58 +0700

    mebuat cek nilai

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 function cekNilai() {
- //mengambil value dari elemen input nilai 
- //simpan ke dalam variabel bernama "nilai"
- let nilai = Number(document.getElementById ('nilai').value)
- 
- //menginisiasi variabel "hasil" isinya string kosong ""
- let hasil = ""
- //menentukan angka A B C D berdasarkan isi variabel "nilai"
- //menggunakan percabangan if (jika)
- //dan membandingkan variabel "nilai"
- if (nilai >=88) {
-  hasil ="nilai A (sangat baik)"
-  } else if (nilai >=75) {
-   hasil ="nilai B (baik)"
-  } else if (nilai >= 50) {
-   hasil ="nilai C (kurang)"
-  } else if (nilai >= 0) {
-   hasil ="nilai D (kurang)"
+  //mengambil value dari elemen input nilai
+  //simpan ke dalam variabel bernama "nilai"
+  let nilai = Number(document.getElementById('nilai').value)
+  
+  // menginisiasi variabel "hasil" isinya string kosong ""
+  let hasil = ""
+  
+  // menentukan angka A B C D berdasarkan isi variabel "nilai"
+  // menggunakan percabangan if (jika)
+  // dan membandingkan variabel "nilai"
+  if (nilai >=88) {
+    hasil = "Nilai A (sangat Baik)"
+  } else if (nilai >= 75) {
+    hasil = "Nilai B (Baik)"
+  } else if (nilai >= 50){
+    hasil = "Nilai C (kurang)"
+  } else {
+    hasil = "Nilai D (sangat kurang)"
   }
-  //tampilkan isi variabel "hasil" ke elemen "hasil" di html
-  document.getElementById('hasil').innerText=hasil
+  // tampilkan isi variabel "hasil" ke elemen "hasil" di html
+document.getElementById('hasil').innerText = hasil
 }
